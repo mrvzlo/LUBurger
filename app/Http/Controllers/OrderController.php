48f77899d74b1041c4ea997f300cd45f1a6f226c
Vladimir <vzlobot01@mail.ru>
2018-06-19 15:03:52 +0300

    heroku 30

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\order;
 use App\dish;
-use App\ord_Dish;
+use App\ord_dish;
 use App\user;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
